5b3d03826014def188ba26b6e9021e3d25601ed1
arinaitweallan <allan.bnb2@gmail.com>
2026-04-15 22:06:36 +0300

    tests

--- test/StakingFarmTest.t.sol
+++ test/StakingFarmTest.t.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.19;
 import {Base} from "test/Base.t.sol";
 import {StakingFarm} from "src/Initialization/StakingFarm.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Token} from "test/mocks/Token.sol";
 
 contract StakingFarmTest is Base {
     StakingFarm staking;
+    Token stakingToken;
+    Token rewardToken;
 
     function setUp() external {
-        staking = new StakingFarm();
+        stakingToken = new Token("stk", "stk");
+        rewardToken = new Token("rwd", "rwd");
+        staking = new StakingFarm(address(stakingToken), address(rewardToken));
     }
 }
